d2d32b2e4929b15cb7d041a93d0792a19f11c9ae
suliman <sulimanalkous@gmail.com>
2019-02-06 11:28:05 +0300

    fix layouts to semantic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
     {
-      success: "alert-success",
-      error: "alert-danger",
-      alert: "alert-warning",
-      notice: "alert-info"
+      success: "ui success message",
+      error: "ui negative message",
+      alert: "ui warning message",
+      notice: "ui info message"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 end
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,7 +4,8 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg) }.join
     html = <<-HTML
-    <div class="alert alert-danger">
+    <div class="ui negative message">
+      <i class="close icon"></i>
       #{messages}
     </div>
     HTML
